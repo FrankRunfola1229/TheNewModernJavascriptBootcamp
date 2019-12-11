87cc05a7a562256746bf0fc3065771b36cdcdd33
Runfola,Frank <runfola.frank@healthnow.org>
2019-12-11 14:16:25 -0500

    unzipped movies

--- Section 17 - HTTP Requests/08-Axios-Intro/app.js
+++ Section 17 - HTTP Requests/08-Axios-Intro/app.js
@@ -1,26 +1,26 @@
 axios
-    .get("https://swapi.co/api/planets/")
-    .then(res => {
-        //We don't have to parse the JSON!
-        console.log(res.data);
-    })
-    .catch(err => {
-        console.log("IN CATCH CALLBACK!!!");
-        console.log(err);
-    });
+	.get('https://swapi.co/api/planets/')
+	.then((res) => {
+		//We don't have to parse the JSON!
+		console.log(res.data);
+	})
+	.catch((err) => {
+		console.log('IN CATCH CALLBACK!!!');
+		console.log(err);
+	});
 
 axios
-    .get("https://swapi.co/api/planetaslkjdaklsjds/") //BAD URL!
-    .then(res => {
-        //We don't need to check for a 200 status code, because...
-        //Axios will reject the promise for us, unlike fetch!
-        console.log(res.data);
-    })
-    .catch(err => {
-        //In this example with a not-found URL, this callback will run...
-        console.log("IN CATCH CALLBACK!!!");
-        console.log(err);
-    });
+	.get('https://swapi.co/api/planetaslkjdaklsjds/') //BAD URL!
+	.then((res) => {
+		//We don't need to check for a 200 status code, because...
+		//Axios will reject the promise for us, unlike fetch!
+		console.log(res.data);
+	})
+	.catch((err) => {
+		//In this example with a not-found URL, this callback will run...
+		console.log('IN CATCH CALLBACK!!!');
+		console.log(err);
+	});
 
 // ********************************
 // USING FETCH INSTEAD...
